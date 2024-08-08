fa511fc6fc973d10b13c922991eabe915cae75c1
Jose <joselopez@popoyan.com>
2024-08-08 15:21:53 -0600

    agregar casos de update y alter y hacer backup

--- CasosDeNegocio.sql
+++ CasosDeNegocio.sql
@@ -92,3 +92,71 @@ where client_id=1
 limit 1;  
 
 TRUNCATE TABLE nombre_de_la_tabla;
+update authors set nationality = 'GBR' where nationality
+= 'ENG';
+
+
+
+----------------------------------------------------
+--EXTRAS
+
+--contar libros mayores para 1950 y los menores
+
+select count(book_id), sum(if(year<1950,1,0)) as '<1950' from books;
+
+select count(book_id), 
+    sum(if(year<1950,1,0)) as '<1950',
+    sum(if(year<1950,0,1)) as '>1950'
+from books;
+
+-- dividir los libros totales segun periodo e este caso 1950 a 1990 a 2200 y hoy
+select count(book_id), 
+    sum(if(year<1950,1,0)) as '<1950',
+    sum(if(year >= 1950 and year <1990,1,0)) as '<1990',
+    sum(if(year >= 1990 and year <2000,1,0)) as '<2000',
+    sum(if(year >= 2000,1,0)) as '<hoy'
+from books;
+
+--Seleccionar libros por periodos de años, pero segun su nacionalidad
+
+select nationality, count(book_id), 
+    sum(if(year<1950,1,0)) as '<1950',
+    sum(if(year >= 1950 and year <1990,1,0)) as '<1990',
+    sum(if(year >= 1990 and year <2000,1,0)) as '<2000',
+    sum(if(year >= 2000,1,0)) as '<hoy'
+from books as b
+Join authors as a
+    on a.author_id = b.author_id
+where
+    a.nationality is not null
+group by a.nationality;
+
+
+
+-----------------------------------------------
+--alter tables
+
+alter table authors add column birthyear integer default 1930 after name;
+
+
+mysql> alter table authors modify column birthyear
+ year default 1920;
+
+
+ alter table authors drop column birthyear;
+
+
+ -------------------------------------------------
+ --mysqldump
+
+--es para hacer backup y debe de hacerse fuera de mysql, recomendable en la carpeta donde se guardara el backup.
+
+ mysqldump -u root -p pruebaplatzi
+
+ mysqldump -u root -p -d pruebaplatzi | more
+
+ --una vez creamos el esquema para exportarlo seria el siguiente comando, y debemos de estar en la carpeta donde estara el documento
+
+mysqldump -u root -p -d pruebaplatzi > esquema.sql
+
+
